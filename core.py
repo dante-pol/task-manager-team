ed15ec574623c4d950f2b8a0fa2ecbb6b291352b
Tumani1337 <idi_suda06@mail.ru>
2024-12-27 21:58:58 +0300

    fix function add_task

--- core.py
+++ core.py
@@ -1,2 +1,19 @@
-def add_task(id: int,title: str, description: str, status: str):
-    pass
+from random import randint
+import os
+
+def add_task(title: str, description: str, status='undone'):
+    Folder = 'datas'
+    exit = True
+    while exit == True:
+        id = str(randint(1000, 9999))
+        if Folder not in os.listdir('task-manager-team'):
+            os.makedirs(Folder)
+        if id in os.listdir(Folder):
+            exit = True
+        else:
+            exit = False
+    file = open(os.path.join(Folder, id), 'w')
+    file.write(f'{id};{title};{description};{status}')
+
+
+
