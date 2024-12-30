3e09911391e30f41f7b8a7bb1dbf3d920e45ec3f
bardoor <dmitrybartenev58@gmail.com>
2024-12-30 21:23:11 +0300

    initial commit

--- core.py
+++
@@ -1,2 +0,0 @@
-def edit_task(index, new_title, new_desc):
-    pass
---
+++ src/string_function.py
@@ -0,0 +1,10 @@
+text = 'Новый год'
+target = input('Введите символ для поиска >> ')
+
+
+def search(text, target):
+    for i in range(len(text)):
+        if text[i] == target:
+            print('True')
+        else:
+            print('False')
