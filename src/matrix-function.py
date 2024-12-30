387ee72233a41da4440131cedad82c874d722faa
Tumani1337 <idi_suda06@mail.ru>
2024-12-30 21:24:12 +0300

    добавил функцию prod_matrix

--- src/matrix-function.py
+++ src/matrix-function.py
@@ -25,3 +25,12 @@ def diff_matrix(matrix1: list[list[int]], matrix2: list[list[int]]) -> list[list
         for j in range(len(matrix1[i])):
             matrix3[i].append(matrix1[i][j] - matrix2[i][j])
     return matrix3
+
+def prod_matrix(matrix1: list[list[int]], number: int) -> list[list[int]]:
+    matrix3 = []
+    for i in range(len(matrix1)):
+        matrix3.append([])
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[i])):
+            matrix3[i].append(matrix1[i][j] * number)
+    return matrix3
