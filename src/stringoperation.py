05b1b94f046f1ef2c47488bab5d9c2e321b0500e
Stanislav Zaburunov <dreamslav@yandex.ru>
2024-12-30 21:36:10 +0300

    fix: добавлена документация для трех функция

--- src/stringoperation.py
+++ src/stringoperation.py
@@ -1,9 +1,12 @@
+#  функция для конкатинации двух строк: str1 и str2
 def concatenation(str1: str,str2: str ) -> str:
     return str1 + str2
 
-def repetition(str3: str, num: int) -> str:
-    return str3 * num
+#  функция для овторения строки str3  num раз
+def repetition(str3: str, num1: int) -> str:
+    return str3 * num1
 
+#  функция для поиска str4  в str5
 def search_inline(str4: str, str5: str) -> bool:
     if str4 in str5:
         return True
