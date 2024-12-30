1c509daadee8333dca773be4f1c9ceb0fde19921
Tumani1337 <idi_suda06@mail.ru>
2024-12-30 21:18:05 +0300

    создал файл matrix-function.py и добавил в него функцию sum_matrix

---
+++ src/matrix-function.py
@@ -0,0 +1,13 @@
+def sum_matrix(matrix1: list[list[int]], matrix2: list[list[int]]) -> list[list[int]]:
+    matrix3 = []
+    if len(matrix1) != len(matrix2):
+        return None
+    for i in range(len(matrix1)):
+        if len(matrix1[i]) != len(matrix2[i]):
+            return None
+    for i in range(len(matrix1)):
+        matrix3.append([])
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[i])):
+            matrix3[i].append(matrix1[i][j] + matrix2[i][j])
+    return matrix3
